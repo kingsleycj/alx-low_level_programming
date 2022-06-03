7b0e8c46bb123a33a670ea3a3c809bfa17f83326
kingsleycj <primekings.kc@gmail.com>
2022-06-02 20:19:36 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,9 @@
  * main - Entry point
  * Return: Always 1 (Success)
  */
+int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
