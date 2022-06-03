7ed516ee58d97316e32d1397aa638b7c410e9913
kingsleycj <primekings.kc@gmail.com>
2022-06-02 20:35:12 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(8, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
