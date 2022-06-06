2052a61786c5acc07df53c5156b7c596919f749f
kingsleycj <primekings.kc@gmail.com>
2022-06-05 18:13:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ *  * main - use to return the integer value 1
+ *   *
+ *    * Description: use to print to standard output in the main function
+ *     *
+ *      * Return: return the value 1
+ *       *
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
 }
